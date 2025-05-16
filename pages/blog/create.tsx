229d3a0147f6dcda02402dd9bd6dc6a163540bf6
shunyahi <shunya.hieda@japan-marketing.co.jp>
2025-05-16 13:51:57 +0900

    ブログページのデータ取得機能を追加し、ブログ作成フォームを実装

---
+++ pages/blog/create.tsx
@@ -0,0 +1,53 @@
+import { useEffect,useState } from "react";
+import{Header} from "../../components/Header.js";
+import { useRouter } from "next/router";
+
+export default function Create() {
+    const router = useRouter();
+    const [title, setTitle] = useState("");
+    const [body, setbody] = useState("");
+const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    try {
+        const res = await fetch("http://127.0.0.1:5000/api/blog", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ "title":title, "body": body }),
+        });
+
+        if (!res.ok) {
+            console.error("Error: Failed to fetch data from API");
+            return;
+        }
+
+        const result = await res.json();
+
+        
+
+        router.push({
+            pathname: "/blog"
+        });
+    } catch (error) {
+        console.error("Error occurred:", error);
+    }
+};
+return(
+<div className="appmoji">
+      <Header />
+      <div className="center"> 
+      <div style={{fontSize: "30px"}}>blog作成フォーム</div>
+     
+
+      <form onSubmit={handleSubmit}>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="input-field" placeholder="タイトルを入力して" ></input>
+        <input type="text" value={body} onChange={(e) => setbody(e.target.value)} className="input-field" placeholder="内容を入力して" ></input>
+      <button type="submit" className="button">取得</button>
+      </form>
+
+      </div>
+    </div>
+
+
+);
+}
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,56 @@
+import { useEffect,useState } from "react";
 import{Header} from "../../components/Header.js";
+import Link from "next/link.js";
+type BlogData = {
+  id: number;
+  title: string;
+  body: string;
+  createdAt: string;
+}
 
 export default function Blog() {
+
+  const [blog, setBlog] = useState<BlogData[]>([]);
+
+  // Fetch data from the API
+  const fetchData = async () => {
+      try {
+          const res = await fetch("http://127.0.0.1:5000/api/blog", {
+              method: "GET",
+              headers: { "Content-Type": "application/json" },
+          });
+
+          if (!res.ok) {
+              console.error("Error: Failed to fetch data from API");
+              return;
+          }
+
+          const result = await res.json();
+          setBlog(result);
+          
+      } catch (error) {
+          console.error("Error occurred:", error);
+      }
+  };
+  
+
+  // useEffectでfetchDataを呼び出す
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       
       <Header />  
-  <div>blog</div>
+      <Link href="/blog/create" style={{ textDecoration: "none", color: "black" }} >ブログ作成ページ</Link>
+      {blog.map((item) => (
+        <div key={item.id} style={{ color: "black" }}>
+          <h2>{item.title}</h2>
+          <p>{item.body}</p>
+          <p>{item.createdAt}</p>
+        </div>
+      ))}
    </div>
 
   );
